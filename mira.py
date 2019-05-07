1537b3c8737e018d6ab3f403b2b4a70338e1df69
sermiki <miki.dino05@gmail.com>
2019-05-07 11:47:14 -0400

    contador de digitos

--- mira.py
+++ mira.py
@@ -1,2 +1,12 @@
-esta creandose archivO
-JUNTO CON OTRO
+
+def cantidad(num):
+    n=num
+    cont=0
+    while (num>0):
+        num=num/10
+        cont+=1
+    print("la cantidad del numero es", cont)
+        
+n=int(input("escribe el numero"))
+numero=cantidad(n)
+
